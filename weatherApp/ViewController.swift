a354040ed5f3d310b31e2806ebbcb2393344b918
Spencer Casteel <spencercasteel@gmail.com>
2018-10-25 14:33:32 -0500

    the code

--- weatherApp/ViewController.swift
+++ weatherApp/ViewController.swift
@@ -7,14 +7,51 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let apiKeys = APIKeys()
+        
+        let darkSkyURL = "https://api.darksky.net/forecast/"
+        
+        let darkSkyKey = apiKeys.darkSkyKey
+        let latitude = "37.004843"
+        let longitude = "-85.925876"
+        
+        let url = darkSkyURL + darkSkyKey + "/" + latitude + "," + longitude
+        
+        print(url)
+        let request = Alamofire.request(url)
+        
+        request.responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print(json)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+        
+        let googleBaseURL = "https://maps.googlepis.com/maps/api/geocode/json?address="
+        let googleRequestURL = googleBaseURL + "Glasgow,+Kentucky" + apiKeys.googleKey
+        let googleRequest = Alamofire.request(googleRequestURL)
+        
+        googleRequest.responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print(json)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
-
-
 }
 
