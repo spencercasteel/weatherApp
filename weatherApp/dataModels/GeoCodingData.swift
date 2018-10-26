dd5418516d09d7495b57bf31f1b0df7e3524d4f3
Spencer Casteel <spencercasteel@gmail.com>
2018-10-26 14:31:08 -0500

    organized project files, and finished weather data class

---
+++ weatherApp/dataModels/GeoCodingData.swift
@@ -0,0 +1,13 @@
+//
+//  GeoCodingData.swift
+//  weatherApp
+//
+//  Created by Spencer Casteel on 10/26/18.
+//  Copyright © 2018 Spencer Casteel. All rights reserved.
+//
+
+import Foundation
+
+class GeoCodingData {
+
+}
